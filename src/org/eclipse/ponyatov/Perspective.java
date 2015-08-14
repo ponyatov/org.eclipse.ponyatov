266d3c318272320bcc61ff7de4c388b222e64534
Dmitry Ponyatov <dponyatov@gmail.com>
2015-08-14 13:30:57 +0400

    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

--- src/org/eclipse/ponyatov/Perspective.java
+++ src/org/eclipse/ponyatov/Perspective.java
@@ -5,9 +5,20 @@ import org.eclipse.ui.IPerspectiveFactory;
 
 public class Perspective implements IPerspectiveFactory {
 
+//	public static final String ID = "org.eclipse.ponyatov.perspective";
+
 	public void createInitialLayout(IPageLayout layout) {
-//		layout.setEditorAreaVisible(false);
-//		layout.setFixed(true);
+//		layout.setEditorAreaVisible(true);
+//		// layout.setFixed(true);
+//		REG.revertPerspective(DESC);
 	}
+
+//	public static IPerspectiveRegistry REG;
+//	public static IPerspectiveDescriptor DESC;
+//
+//	public Perspective() {
+//		REG = PlatformUI.getWorkbench().getPerspectiveRegistry();
+//		DESC = REG.findPerspectiveWithId(ID);
+//	}
 
 }
