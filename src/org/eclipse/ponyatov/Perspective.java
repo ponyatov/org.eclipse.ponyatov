559b0d6e44940318093f04d5deccccdfbb131d03
Dmitry Ponyatov <dponyatov@gmail.com>
2015-08-14 12:59:37 +0400

    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

--- src/org/eclipse/ponyatov/Perspective.java
+++ src/org/eclipse/ponyatov/Perspective.java
@@ -1,14 +1,13 @@
 package org.eclipse.ponyatov;
 
-//import org.eclipse.ui.IPageLayout;
-//import org.eclipse.ui.IPerspectiveFactory;
-//
-//public class Perspective implements IPerspectiveFactory {
-//
-//	public void createInitialLayout(IPageLayout layout) {
+import org.eclipse.ui.IPageLayout;
+import org.eclipse.ui.IPerspectiveFactory;
+
+public class Perspective implements IPerspectiveFactory {
+
+	public void createInitialLayout(IPageLayout layout) {
 //		layout.setEditorAreaVisible(false);
 //		layout.setFixed(true);
-//		
-//	}
-//
-//}
+	}
+
+}
