36e4a5520ef199a2759d12c0e366dd287abd9866
Dmitry Ponyatov <dponyatov@gmail.com>
2015-08-16 12:06:13 +0400

    * makebuilder, makenature

--- src/org/eclipse/ponyatov/Perspective.java
+++ src/org/eclipse/ponyatov/Perspective.java
@@ -3,10 +3,10 @@ package org.eclipse.ponyatov;
 import org.eclipse.core.resources.IWorkspace;
 import org.eclipse.core.resources.IWorkspaceDescription;
 import org.eclipse.core.resources.ResourcesPlugin;
-import org.eclipse.core.runtime.CoreException;
 import org.eclipse.ui.IFolderLayout;
 import org.eclipse.ui.IPageLayout;
 import org.eclipse.ui.IPerspectiveFactory;
+import org.eclipse.ui.IPlaceholderFolderLayout;
 import org.eclipse.ui.console.IConsoleConstants;
 
 public class Perspective implements IPerspectiveFactory {
@@ -22,15 +22,15 @@ public class Perspective implements IPerspectiveFactory {
 		// get workspace
 		workspace = ResourcesPlugin.getWorkspace();
 		description = workspace.getDescription();
-		// set autobuild flag
-		if (!description.isAutoBuilding()) description.setAutoBuilding(true);
-		// update workspace
-		try {
-			workspace.setDescription(description);
-		} catch (CoreException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+//		// set autobuild flag
+//		if (!description.isAutoBuilding()) description.setAutoBuilding(true);
+//		// update workspace
+//		try {
+//			workspace.setDescription(description);
+//		} catch (CoreException e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		}
 	}
 	
 	public void defineActions(IPageLayout layout) {
@@ -42,17 +42,21 @@ public class Perspective implements IPerspectiveFactory {
 		layout.addShowViewShortcut(IPageLayout.ID_OUTLINE);
 		layout.addShowViewShortcut(IPageLayout.ID_PROP_SHEET);
 		layout.addShowViewShortcut(IPageLayout.ID_TASK_LIST);
-//		layout.addShowViewShortcut(IPageLayout.ID_PROBLEM_VIEW);
+		layout.addShowViewShortcut(IPageLayout.ID_PROBLEM_VIEW);//ID_GIT
 		layout.addShowViewShortcut(IConsoleConstants.ID_CONSOLE_VIEW);
 	}
 
 	public void defineLayout(IPageLayout layout) {
 		String editorArea = layout.getEditorArea();
-		IFolderLayout left = layout.createFolder("left", IPageLayout.LEFT, (float) 0.2, editorArea);
+		IPlaceholderFolderLayout xxx = layout.createPlaceholderFolder("left", IPageLayout.LEFT, (float) 0.2, editorArea);
+		IFolderLayout leftup = layout.createFolder("left", IPageLayout.LEFT, (float) 0.2, editorArea);
+		xxx.addPlaceholder(IPageLayout.ID_PROBLEM_VIEW);
+//		IFolderLayout leftdn = layout.createFolder("left", IPageLayout.LEFT, (float) 0.5, leftup);
 		IFolderLayout right = layout.createFolder("right", IPageLayout.RIGHT, (float) 0.75, editorArea);
 		IFolderLayout bottom = layout.createFolder("bottom", IPageLayout.BOTTOM, (float) 0.7, editorArea);
 //		bottom.addPlaceholder(IConsoleConstants.ID_CONSOLE_VIEW);
-		left.addView(IPageLayout.ID_PROJECT_EXPLORER);
+		leftup.addView(IPageLayout.ID_PROJECT_EXPLORER);
+//		leftdn.addView(IPageLayout.ID_PROBLEM_VIEW);//ID_GIT
 		right.addView(IPageLayout.ID_OUTLINE);
 //		bottom.addView(IPageLayout.ID_PROBLEM_VIEW);
 		bottom.addView(IConsoleConstants.ID_CONSOLE_VIEW);
