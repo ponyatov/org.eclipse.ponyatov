f8d8d0494170d1612927faa26259e439b03ea51f
Dmitry Ponyatov <dponyatov@gmail.com>
2015-08-16 17:01:24 +0400

    * nature/builder
    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

--- src/org/eclipse/ponyatov/MakeBuilder.java
+++ src/org/eclipse/ponyatov/MakeBuilder.java
@@ -8,6 +8,19 @@ import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IProgressMonitor;
 
 public class MakeBuilder extends IncrementalProjectBuilder {
+	
+	public MakeBuilder() {
+		// TODO Auto-generated constructor stub
+		super();
+		System.out.println("Custom builder consturctoed");
+	}
+	
+	@Override
+	protected void clean(IProgressMonitor monitor) throws CoreException {
+		// TODO Auto-generated method stub
+		super.clean(monitor);
+		System.out.println("make clean");
+	}
 
 	@Override
 	protected IProject[] build(int kind, Map<String, String> arg1, IProgressMonitor arg2) throws CoreException {
--- src/org/eclipse/ponyatov/MakeNature.java
+++ src/org/eclipse/ponyatov/MakeNature.java
@@ -1,5 +1,40 @@
 package org.eclipse.ponyatov;
 
-public class MakeNature {
+import org.eclipse.core.resources.IProject;
+import org.eclipse.core.resources.IProjectNature;
+import org.eclipse.core.runtime.CoreException;
+
+public class MakeNature implements IProjectNature {
+	
+	public static final String ID="org.eclipse.ponyatov.makenature";
+	
+	MakeNature() {
+		super();
+		System.out.println(ID);
+	}
+
+	@Override
+	public void configure() throws CoreException {
+		// TODO Auto-generated method stub
+		System.out.println(ID+" configure");
+	}
+
+	@Override
+	public void deconfigure() throws CoreException {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	private IProject project;
+
+	@Override
+	public IProject getProject() {
+		return project;
+	}
+
+	@Override
+	public void setProject(IProject arg0) {
+		project = arg0;
+	}
 
 }
